68e173bbe77935d3b108ca36ff5f5ab477fda46b
DmitriiVKh <hdv.kub@gmail.com>
2023-12-02 15:46:29 +0300

    JSON Placeholder

---
+++ Todo-List/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await response.json();
+      setTodos(data);
+    } catch (error) {
+      console.log('Ошибка при получении данных', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+  
+
+  return (
+    <>
+      <div className="todo-list">
+        <h1>Todo List</h1>
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
